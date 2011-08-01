58e2a079900f634a46d1a32173be3b31b8a7e46e
Juan Antonio Martinez <jonsito@terra.es>
2011-08-01 13:15:49 +0200

    fix missing braces in opensc-tool.c

--- src/tools/opensc-tool.c
+++ src/tools/opensc-tool.c
@@ -347,7 +347,7 @@ static int print_file(sc_card_t *in_card, const sc_file_t *file,
 			{ SC_FILE_EF_LINEAR_VARIABLE,     "linear-variable"       },
 			{ SC_FILE_EF_LINEAR_VARIABLE_TLV, "linear-variable (TLV)" },
 			{ SC_FILE_EF_CYCLIC,              "cyclic"                },
-			{ SC_FILE_EF_CYCLIC_TLV,          "cyclic (TLV)"  
+			{ SC_FILE_EF_CYCLIC_TLV,          "cyclic (TLV)" 	  }, 
 			{ 0, NULL }
 		};
 		const char *ef_type = "unknown";
